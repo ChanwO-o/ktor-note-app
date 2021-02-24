f41298c49a8c360e0fb02eb6ce0b5ecf49764cf7
Chan Woo Park <14cpark@gmail.com>
2021-02-24 00:06:43 -0800

    show notes in NotesFragment

--- app/src/main/java/com/androiddevs/ktornoteapp/ui/notes/NotesFragment.kt
+++ app/src/main/java/com/androiddevs/ktornoteapp/ui/notes/NotesFragment.kt
@@ -1,16 +1,22 @@
 package com.androiddevs.ktornoteapp.ui.notes
 
 import android.content.SharedPreferences
+import android.content.pm.ActivityInfo
 import android.os.Bundle
 import android.view.*
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
 import androidx.navigation.NavOptions
 import androidx.navigation.Navigation.findNavController
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.androiddevs.ktornoteapp.R
+import com.androiddevs.ktornoteapp.adapters.NoteAdapter
 import com.androiddevs.ktornoteapp.other.Constants.KEY_LOGGED_IN_EMAIL
 import com.androiddevs.ktornoteapp.other.Constants.KEY_LOGGED_IN_PASSWORD
 import com.androiddevs.ktornoteapp.other.Constants.NO_EMAIL
 import com.androiddevs.ktornoteapp.other.Constants.NO_PASSWORD
+import com.androiddevs.ktornoteapp.other.Status
 import com.androiddevs.ktornoteapp.ui.BaseFragment
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.fragment_notes.*
@@ -19,8 +25,12 @@ import javax.inject.Inject
 @AndroidEntryPoint
 class NotesFragment: BaseFragment(R.layout.fragment_notes) {
 
+	private val viewModel: NotesViewModel by viewModels()
+
 	@Inject
 	lateinit var sharedPref: SharedPreferences
+
+	private lateinit var noteAdapter: NoteAdapter
 
 	override fun onCreateView(
 		inflater: LayoutInflater,
@@ -29,13 +39,70 @@ class NotesFragment: BaseFragment(R.layout.fragment_notes) {
 	): View? {
 		setHasOptionsMenu(true) // implement menu on fragment
 		return super.onCreateView(inflater, container, savedInstanceState)
+
+		// re-enable screen rotation: we can't do it directly inside a fragment. so
 	}
 
 	override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
 		super.onViewCreated(view, savedInstanceState)
+
+		// in AuthFragment we disabled screen rotation. Now we want to re-enable it
+		requireActivity().requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_USER
+
+		setupRecyclerView()
+		subscribeToObservers()
+
+		noteAdapter.setOnItemClickListener {
+			findNavController().navigate(
+				NotesFragmentDirections.actionNotesFragmentToNoteDetailFragment(it.id)
+			)
+		}
+
 		fabAddNote.setOnClickListener {
 			findNavController().navigate(NotesFragmentDirections.actionNotesFragmentToAddEditNoteFragment(""))
 		}
+	}
+
+	private fun subscribeToObservers() {
+		viewModel.allNotes.observe(viewLifecycleOwner, Observer {
+			it?.let { event ->
+				// save content of the event to variable
+				val result = event.peekContent() // use peekContent() so that we don't consume the event
+				when (result.status) {
+					Status.SUCCESS -> {
+						noteAdapter.notes = result.data!! // assume != null since Success state never gives null
+						swipeRefreshLayout.isRefreshing = false // stop loading
+					}
+					Status.ERROR -> {
+						// this is the only scenario where we want to consume the event (show on snackbar)
+						event.getContentIfNotHandled()?.let { errorResource ->
+							// we only go inside this let block the first time we get an Error resource
+							errorResource.message?.let { message ->
+								showSnackbar(message)
+							}
+						}
+						// when fetch data fails, we still get data from db
+						result.data?.let { notes ->
+							noteAdapter.notes = notes
+						}
+						swipeRefreshLayout.isRefreshing = false // stop loading
+					}
+					Status.LOADING -> {
+						result.data?.let { notes ->
+							// when we fetch data, we already got data from database, so we can display it even though we're still loading
+							noteAdapter.notes = notes
+						}
+						swipeRefreshLayout.isRefreshing = true
+					}
+				}
+			}
+		})
+	}
+
+	private fun setupRecyclerView() = rvNotes.apply {
+		noteAdapter = NoteAdapter()
+		adapter = noteAdapter
+		layoutManager = LinearLayoutManager(requireContext())
 	}
 
 	// navigate back to AuthFragment, delete login entry in SharedPreferences
--- app/src/main/java/com/androiddevs/ktornoteapp/ui/notes/NotesViewModel.kt
+++ app/src/main/java/com/androiddevs/ktornoteapp/ui/notes/NotesViewModel.kt
@@ -1,7 +1,35 @@
 package com.androiddevs.ktornoteapp.ui.notes
 
-import androidx.lifecycle.ViewModel
+import androidx.hilt.lifecycle.ViewModelInject
+import androidx.lifecycle.*
+import com.androiddevs.ktornoteapp.data.local.entities.Note
+import com.androiddevs.ktornoteapp.other.Event
+import com.androiddevs.ktornoteapp.other.Resource
+import com.androiddevs.ktornoteapp.repositories.NoteRepository
 
-class NotesViewModel: ViewModel() {
 
+// getting all notes via this ViewModel:
+// 1. we want to retrieve the data once, so that we don't make the request again when the device is rotated
+// 2. we want to be able to sync when we want, where we will be making a request
+// solution: use _forceUpdate: post a value to _forceUpdate to get actual LiveData (the Flow LD that returns List of Notes)
+
+class NotesViewModel @ViewModelInject constructor(
+	private val repository: NoteRepository
+): ViewModel() {
+
+	private val _forceUpdate = MutableLiveData<Boolean>(false)
+
+	private val _allNotes = _forceUpdate.switchMap {
+		// whenever a value is posted to _forceUpdate, emit this block
+		repository.getAllNotes().asLiveData(viewModelScope.coroutineContext)
+		// to make sure this gets triggered once, we wrap it around our Event class (to handle one-time events)
+		// to do this, use another switchMap on the LiveData again!
+	}.switchMap {
+		// map to MutableLD of type event
+		MutableLiveData(Event(it)) // 'it' refers to the resource of list of Notes from the database
+	}
+	// now make an immutable version of that LD
+	val allNotes: LiveData<Event<Resource<List<Note>>>> = _allNotes
+
+	fun syncAllNotes() = _forceUpdate.postValue(true)
 }
